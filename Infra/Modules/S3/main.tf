f05a87016807bf7bd2fffab33d94b50ae0412463
ajay surwase <surwaseajay07@gmail.com>
2025-05-02 21:51:42 +0530

    Adding Ec2 module to repo

---
+++ Infra/Modules/S3/main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "${var.bucket_name}-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.bucket_name}-lock"
+  }
+}
